6d7f4524d616b3c0815706b0177cdf30c314d050
Quinn-H <quinn.hu618@gmail.com>
2017-03-02 04:53:49 +1300

    first commit

---
+++ FizzBuzz/fizzbuzz.js
@@ -0,0 +1,12 @@
+module.exports = {
+  solution: function (number) {
+  var sum = 0
+
+  for(var i = 1;i< number; i++){
+    if(i % 3 == 0 || i % 5 == 0){
+      sum += i
+    }
+  }
+  return sum
+}
+}
---
+++ FizzBuzz/fizzbuzz_test.js
@@ -0,0 +1,11 @@
+var assert = require('assert')
+var fizzbuzz = require('./fizzbuzz.js')
+var solution = fizzbuzz.solution
+
+
+describe('solution', function() {
+  it('result should be 23', function() {
+    var answer = 23
+    expect(answer).to.equal(23);
+  })
+})
