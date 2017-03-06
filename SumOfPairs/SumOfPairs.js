0b11ce12f76211176ec97cdca63e6f17a9602518
Quinn-H <quinn.hu618@gmail.com>
2017-03-06 23:43:02 +1300

    add another solution

--- SumOfPairs/SumOfPairs.js
+++ SumOfPairs/SumOfPairs.js
@@ -11,3 +11,12 @@ var sum_pairs=function(ints, s){
       }
       }
 }
+
+// This solution is so smart
+var sum_pairs=function(ints, s){
+  var seen = {}
+  for (var i = 0; i < ints.length; ++i) {
+    if (seen[s - ints[i]]) return [s - ints[i], ints[i]];
+    seen[ints[i]] = true
+  }
+}
